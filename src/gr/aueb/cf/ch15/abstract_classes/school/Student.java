400d5206719f42ecee7bea58f601bac7a9902afe
natbonn <natbon@hotmail.gr>
2026-06-06 15:16:35 +0300

    Add Student and Teacher classes extending AbstractActor with specific attributes

---
+++ src/gr/aueb/cf/ch15/abstract_classes/school/Student.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.ch15.abstract_classes.school;
+
+public class Student extends AbstractActor {
+    private String region;
+
+    public Student() {
+    }
+
+    public Student(long id, String firstname, String lastname) {
+        super(id, firstname, lastname);
+        this.region = region;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+}
