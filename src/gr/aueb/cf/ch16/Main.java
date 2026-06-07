67569397762dc9ad8d308ce3d018592241372285
natbonn <natbon@hotmail.gr>
2026-06-07 19:59:29 +0300

    Add Main class to demonstrate Book creation using Builder pattern

---
+++ src/gr/aueb/cf/ch16/Main.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch16;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Book pythonBook = new Book.Builder(1, "A123").build();
+
+        Book javaBook = new Book.Builder(2, "B123")
+                            .title("Java Programming")
+                            .author("John Doe")
+                            .build();
+
+        Book javaScriptBook = new Book.Builder(3, "G135")
+                .title("JavaScript Programming")
+                .build();
+    }
+}
