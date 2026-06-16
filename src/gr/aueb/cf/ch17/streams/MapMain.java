aa300961cc4c03d63c9e971a9467031ff07a9c27
natbonn <natbon@hotmail.gr>
2026-06-16 20:48:55 +0300

    Add ReduceMain class to demonstrate stream reduction; implement total sum and sum of quantities

--- src/gr/aueb/cf/ch17/streams/MapMain.java
+++ src/gr/aueb/cf/ch17/streams/MapMain.java
@@ -3,12 +3,20 @@ package gr.aueb.cf.ch17.streams;
 import gr.aueb.cf.ch17.sorting.Product;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class MapMain {
 
     public static void main(String[] args) {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("GR", "Greece");
+        map.put("FR", "France");
+        map.put("DE", "Germany");
+        map.put("USA", "United States of America");
 
         List<Product> products = new ArrayList<>(List.of(
                 new Product("Apples", 12.7, 40),
@@ -26,6 +34,11 @@ public class MapMain {
                 .collect(Collectors.joining(","));
 
         System.out.println(listOfDescriptions);
+
+        var filteredCities = map.entrySet().stream()
+                .filter(entry -> entry.getValue().length() <= 10)
+                .collect(Collectors.toSet());
+        filteredCities.forEach(System.out::println);
 
     }
 }
