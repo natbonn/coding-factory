37d76e03fa07f5727aebdfe06b0f07ea5be729f8
natbonn <natbon@hotmail.gr>
2026-06-16 17:43:26 +0300

    Add Product class; implement properties and comparison based on price

---
+++ src/gr/aueb/cf/ch17/sorting/Product.java
@@ -0,0 +1,57 @@
+package gr.aueb.cf.ch17.sorting;
+
+public class Product implements Comparable<Product> {
+    private String description;
+    private double price;
+    private int quantity;
+
+    public Product() {
+    }
+
+    public Product(String description, double price, int quantity) {
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public int compareTo(Product o) {
+//        return this.description.compareTo(o.description);
+//        if (this.quantity < o.quantity) {
+//            return -1;
+//        } else if (this.quantity > o.quantity) {
+//            return 1;
+//        } else {
+//            return 0;
+//        }
+//        return this.quantity - o.quantity;    // σύντμηση του πάνω - ίδια λογική
+
+        return Double.compare(this.price, o.price);
+    }
+}
+
+
