c95da49dfa0df62dc602a0928c7bd7fabceb5545
natbonn <natbon@hotmail.gr>
2026-06-16 21:07:26 +0300

    Add UserRole enum and Main class to demonstrate enum usage; implement role retrieval and iteration

---
+++ src/gr/aueb/cf/ch17/enums/Main.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch17.enums;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserRole userRole = UserRole.ADMIN;
+
+        String userRoleStr = userRole.name();    // ADMIN
+        UserRole userRole2 = userRole.valueOf("ADMIN");
+
+        for (UserRole role : UserRole.values()) {
+            System.out.println(role.ordinal() + " - " + role.name());
+        }
+
+
+
+    }
+}
