c928ca76176392fe9f7126fde0a99f005e128545
natbonn <natbon@hotmail.gr>
2026-06-17 20:49:54 +0300

    Add AccountReadOnlyDTO and Main class for bank application; implement account creation menu

---
+++ src/gr/aueb/cf/ch18/bankapp/Main.java
@@ -0,0 +1,60 @@
+package gr.aueb.cf.ch18.bankapp;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main {
+
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String option;
+        String iban;
+        BigDecimal balance;
+
+        while (true) {
+            printMenu();
+            option = scanner.nextLine().trim();
+
+            try {
+                switch (option) {
+                    case "1" -> {
+                        System.out.print("Παρακαλώ εισάγετε το IBAN: ");
+                        iban = scanner.nextLine().trim();
+                        System.out.print("Παρακαλώ εισάγετε το αρχικό υπόλοιπο: ");
+                        balance = new BigDecimal(scanner.nextLine().trim());
+
+                        // AccountReadOnlyDTO readOnlyDTO = accountController.createNewAccount(iban, balance);
+
+
+                    }
+
+
+
+
+                }
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+
+
+    }
+
+    private static void printMenu() {
+        System.out.println("\n=====================");
+        System.out.println("| Υπηρεσία Bank App  |");
+        System.out.println("=====================");
+        System.out.println("\nΥποσύστημα Τράπεζας");
+        System.out.println("    1. Δημιουργία (ή ενημέρωση) λογαριασμού");
+        System.out.println("    2. Προβολή Λογαριασμών");
+        System.out.println("Υποσύστημα Πελατών");
+        System.out.println("    3. Κατάθεση");
+        System.out.println("    4. Ανάληψη");
+        System.out.println("    5. Ερώτηση υπολοίπου");
+        System.out.println("[Qq]  Έξοδος");
+        System.out.print("\n Εισάγετε μία επιλογή: ");
+    }
+}
