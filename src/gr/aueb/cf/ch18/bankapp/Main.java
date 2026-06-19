c3a05fcdd1c165f7a991475fee52bd06e2b5e252
natbonn <natbon@hotmail.gr>
2026-06-19 20:10:35 +0300

    Add deposit and withdraw methods to AccountController; implement account listing in Main

--- src/gr/aueb/cf/ch18/bankapp/Main.java
+++ src/gr/aueb/cf/ch18/bankapp/Main.java
@@ -4,6 +4,7 @@ import gr.aueb.cf.ch18.bankapp.controller.AccountController;
 import gr.aueb.cf.ch18.bankapp.dto.AccountReadOnlyDTO;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -15,6 +16,7 @@ public class Main {
         String option;
         String iban;
         BigDecimal balance;
+
 
         while (true) {
             printMenu();
@@ -32,6 +34,45 @@ public class Main {
                         System.out.println("Ο λογαριασμός δημιουργήθηκε ή ανανεώθηκε επιτυχώς");
                         System.out.println("IBAN: " + readOnlyDTO.iban() + "\nΥπόλοιπο: " + readOnlyDTO.balance());
                     }
+                    case "2" -> {
+                        List<AccountReadOnlyDTO> readOnlyDTOS = accountController.getAllAccounts();
+
+                        if (readOnlyDTOS.isEmpty()) {
+                            System.out.println("\n Δεν βρέθηκαν λογαριασμοί");
+                        } else {
+                            System.out.println("\n--------------------------");
+                            System.out.println("|        Λογαριασμοί        |");
+                            System.out.println("\n--------------------------");
+                            readOnlyDTOS.forEach(System.out::println);
+                            System.out.println();
+                        }
+                    }
+                    case "3" -> {
+                        System.out.print("Παρακαλώ εισάγετε το IBAN: ");
+                        iban = scanner.nextLine().trim();
+                        System.out.print("Παρακαλώ εισάγετε το ποσό κατάθεσης: ");
+                        BigDecimal depositAmount = new BigDecimal(scanner.nextLine().trim());
+
+                        accountController.deposit(iban, depositAmount);
+                        System.out.println("\nΕπιτυχής κατάθεση");
+                        System.out.println("Ποσό κατάθεσης: " + depositAmount + ", Νέο Υπόλοιπο: "); //+
+//                                accountController.getBalance();
+                    }
+                    case "4" -> {
+                        System.out.print("Παρακαλώ εισάγετε το IBAN: ");
+                        iban = scanner.nextLine().trim();
+                        System.out.print("Παρακαλώ εισάγετε το ποσό ανάληψης: ");
+                        BigDecimal withdrawAmount = new BigDecimal(scanner.nextLine().trim());
+
+                        accountController.withdraw(iban, withdrawAmount);
+                        System.out.println("\n Επιτυχής ανάληψη");
+                        System.out.println("Ποσό κατάθεσης: " + withdrawAmount + ", Νέο υπόλοιπο: "); //+
+                        // accountController.getBalance();
+
+                    }
+
+
+
                     default -> System.out.println("\n Μη έγκυρη επιλογή");
                 }
             } catch (Exception e) {
--- src/gr/aueb/cf/ch18/bankapp/controller/AccountController.java
+++ src/gr/aueb/cf/ch18/bankapp/controller/AccountController.java
@@ -11,21 +11,60 @@ import java.util.List;
 public class AccountController {
 
     // dummy - List
-    private final List<Account> accounts = new ArrayList<>();
+//    private final List<Account> accounts = new ArrayList<>();
 
     public AccountReadOnlyDTO createNewAccount(String iban, BigDecimal balance) {
         // Data binding
-        AccountInsertDTO insertDTO = new AccountInsertDTO(iban, balance);
+        // AccountInsertDTO insertDTO = new AccountInsertDTO(iban, balance);
         AccountReadOnlyDTO readOnlyDTO;
+
+        // 1.  Validation
+
+        // 2. Service Call
+        // readOnlyDTO = accountService.createAccount(insertDTO);
+
+        // Dummy Data
+        readOnlyDTO = new AccountReadOnlyDTO(iban, balance);
+        return readOnlyDTO;
+    }
+
+    public void deposit(String iban, BigDecimal amount) {
 
         // Validation
 
-        // Dummy - Extract
-        accounts.add(new Account(iban, balance));
+        // Dummy Data
+        if (iban.equals("GR12345")) {
+            throw new IllegalArgumentException("Account with IBAN: " + iban + " does not exist");
+        }
 
-        // Confirmation
-        readOnlyDTO = new AccountReadOnlyDTO(iban, balance);
-        return readOnlyDTO;
+        // Service Call
+        // accountService.deposit(iban, amount);
+    }
 
+    public void withdraw(String iban, BigDecimal amount) {
+
+        // Validation
+
+        // Dummy Data
+        if (iban.equals("GR12345")) {
+            throw new IllegalArgumentException("Account with IBAN: " + iban + " does not exist");
+        }
+
+        // Service Call
+        // accountService.withdraw(iban, amount);
+    }
+
+    public List<AccountReadOnlyDTO> getAllAccounts() {
+
+        // Validation
+
+        // Dummy Data
+        return List.of(new AccountReadOnlyDTO("GR12345", BigDecimal.valueOf(1000)),
+                new AccountReadOnlyDTO("GR12346", BigDecimal.valueOf(2000)),
+                new AccountReadOnlyDTO("GR12347", BigDecimal.valueOf(3000)),
+                new AccountReadOnlyDTO("GR12348", BigDecimal.valueOf(4000)));
+
+        // Service Call
+        // return accountService.getAllAccounts();
     }
 }
